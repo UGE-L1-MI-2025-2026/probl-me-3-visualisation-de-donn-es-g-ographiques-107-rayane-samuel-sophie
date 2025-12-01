3b91d34054be530b8993ce032786164c0bcbcd4e
RAYANE BELGAT <rayane.belgat@edu.univ-eiffel.fr>
2025-12-01 10:52:58 +0100

    opti

--- test.py
+++ test.py
@@ -1,17 +1,44 @@
-from shapefile import * 
-from fltk import *
+from shapefile import Reader
+from fltk import cree_fenetre, polygone, mise_a_jour, attend_fermeture
+from math import radians, cos, sin
 
 shp = Reader("departements-20180101")
-departements = []
 
-x1,y1,x2,y2 = shp.bbox
-offset_x = abs(x1)
-offset_y = abs(y1)
+rotation_deg = -1
 
+x1, y1, x2, y2 = shp.bbox
+center_x = (x1 + x2) / 2
+center_y = (y1 + y2) / 2
 
-cree_fenetre(500,500,redimension=True)
-points = [((elem1+offset_x)*15, (elem2+offset_y)*15) for elem1, elem2 in shp.shape(1).points ]
-polygone(points)
+win_w, win_h = 1440, 720
+cree_fenetre(win_w, win_h, redimension=True, affiche_repere=True)
+
+zoom = 1
+map_w = x2 - x1
+map_h = y2 - y1
+margin = 100
+scale = (min((win_w - margin) / map_w, (win_h - margin) / map_h)) * zoom
+
+theta = radians(rotation_deg)
+c, s = cos(theta), sin(theta)
+
+screen_cx = win_w / 2
+screen_cy = win_h / 2
+
+for shape in shp.shapes():
+    pts = []
+    for lon, lat in shape.points:
+        dx = (lon - center_x) * cos(radians(center_y)) 
+        dy = (lat - center_y)
+        rx = dx * c - dy * s
+        ry = dx * s + dy * c
+        sx = rx * scale
+        sy = -ry * scale
+
+        screen_x = sx + screen_cx
+        screen_y = sy + screen_cy
+        pts.append((screen_x, screen_y))
+    polygone(pts)
+
 mise_a_jour()
 attend_fermeture()
-ferme_fenetre()
