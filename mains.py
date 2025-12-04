7cb4dbeff7c4067654d5a9768d4d138b78c69d10
rayanebelgat <rayane.belgat@edu.univ-eiffel.fr>
2025-12-04 16:39:06 +0100

    Delete mains.py

--- mains.py
+++
@@ -1,8 +0,0 @@
-from shapefile import Reader
-from fltk import cree_fenetre, polygone, mise_a_jour, attend_fermeture
-from App import Geo
-
-cree_fenetre(1440, 720, affiche_repere=True)
-
-geo = Geo()
-geo.draw_map()
